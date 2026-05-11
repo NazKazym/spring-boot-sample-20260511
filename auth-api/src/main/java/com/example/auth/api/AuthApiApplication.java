e3dddd19031a347c6f68c034d056eace1b44d6d3
NazKazym <kazymovnaz@gmail.com>
2026-05-11 13:37:10 -0600

    Initial commit

---
+++ auth-api/src/main/java/com/example/auth/api/AuthApiApplication.java
@@ -0,0 +1,23 @@
+
+package com.example.auth.api;
+
+
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+
+public class AuthApiApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AuthApiApplication.class, args);
+
+    }
+
+}
+
